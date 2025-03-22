58c2976cdb55b317b2bdd2177942f722de68e4f1
NeetaKP <neetu.koirala@gmail.com>
2025-03-22 19:47:49 -0400

    screen  setup

---
+++ LabAssignment-2/LabAssignment-2/ProductListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductListViewController.swift
+//  LabAssignment-2
+//
+//  Created by Niko Pant on 2025-03-21.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LabAssignment-2/LabAssignment-2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LabAssignment-2
+//
+//  Created by Niko Pant on 2025-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    private let button: UIButton = {
+    
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+                            
+    
+
+    
+
+
+}
+
